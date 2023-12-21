c6e30dacd465fc2a83c39883e2119f9e1946627d
CFC-Tech-AI <pythoncfc@gmail.com>
2023-12-21 19:22:50 +0530

    commit

--- text2video/urls.py
+++ text2video/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", index, name="index"),   
-    path('text_to_video/', text_to_video, name='text_to_video'),
+     
+    path('', text_to_video, name='text_to_video'),
       
 ]
--- text2video/views.py
+++ text2video/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request,"index.html")
-
 
 def text_to_video(request):
     return render(request, 'text2video.html')
